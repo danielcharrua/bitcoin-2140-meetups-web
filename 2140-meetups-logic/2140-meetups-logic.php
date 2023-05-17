33318f9ad3dc4a591a58fba46d80476df943bc2c
Daniel Pereyra Costas <danielp@charrua.es>
2023-05-17 16:37:32 +0200

    use null if no email on community

--- 2140-meetups-logic/2140-meetups-logic.php
+++ 2140-meetups-logic/2140-meetups-logic.php
@@ -645,7 +645,7 @@ function create_btcmap_area($post_id, $post, $update)
         $data = array(
             "id"        => $post->ID,
             "id_btcmap" => sanitize_title(remove_emoji($post->post_title)),
-            "email"     => get_post_meta($post_id, 'email', true),
+            "email"     => get_post_meta($post_id, 'email', true) ? get_post_meta($post_id, 'email', true) : null,
             "telegram"  => get_post_meta($post_id, 'telegram', true),
             "imagen"    => get_the_post_thumbnail_url($post_id, 'full'),
             "nombre"    => $post->post_title,
@@ -692,7 +692,7 @@ function btcmap_get_endpoint_loop_communities()
         $data = array(
             "id"        => $community->ID,
             "id_btcmap" => sanitize_title(remove_emoji($community->post_title)),
-            "email"     => get_post_meta($community->ID, 'email', true),
+            "email"     => get_post_meta($community->ID, 'email', true) ? get_post_meta($community->ID, 'email', true) : null,
             "telegram"  => get_post_meta($community->ID, 'telegram', true),
             "imagen"    => get_the_post_thumbnail_url($community->ID, 'full'),
             "nombre"    => $community->post_title,
